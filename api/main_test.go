7a5b64ec64a2a43f70adffb89b8d4d889f2b285a
Tan Lam Ngan <ngantl@sendo.vn>
2024-02-26 11:18:47 +0700

    implement api login

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,25 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/JackyTaan/spbank/db/sqlc"
+	"github.com/JackyTaan/spbank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	Server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return Server
+}
 
 func TestMain(m *testing.M) {
 	//
